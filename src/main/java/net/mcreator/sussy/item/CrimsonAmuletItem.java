8f7508c79053c12d0c2e8397021a54aadfc0fc58
Chao192 <ethan@DESKTOP-ONI9CPD.attlocal.net>
2023-09-26 14:24:03 -0500

    Changes to Crimson Amulet

--- src/main/java/net/mcreator/sussy/item/CrimsonAmuletItem.java
+++ src/main/java/net/mcreator/sussy/item/CrimsonAmuletItem.java
@@ -10,7 +10,10 @@ import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.network.chat.Component;
 
 import net.mcreator.sussy.procedures.CrimsonAmuletRightclickedProcedure;
@@ -43,6 +46,17 @@ public class CrimsonAmuletItem extends Item {
 		super.appendHoverText(itemstack, world, list, flag);
 		list.add(Component.literal("It resembles a netherack corridor"));
 		list.add(Component.literal("...or maybe an eye"));
+	}
+
+	@Override
+	public InteractionResultHolder<ItemStack> use(Level world, Player entity, InteractionHand hand) {
+		InteractionResultHolder<ItemStack> ar = super.use(world, entity, hand);
+		ItemStack itemstack = ar.getObject();
+		double x = entity.getX();
+		double y = entity.getY();
+		double z = entity.getZ();
+		CrimsonAmuletRightclickedProcedure.execute(entity);
+		return ar;
 	}
 
 	@Override
