2bca48a38038c4fdd289da9c959fda36dbaab195
Carsoa5 <bands@LAPTOP-R30RBCTK>
2023-10-05 15:03:31 -0500

    reworked roobeer again

--- src/main/java/net/mcreator/sussy/procedures/ROOBEEREffectEffectStartedappliedProcedure.java
+++ src/main/java/net/mcreator/sussy/procedures/ROOBEEREffectEffectStartedappliedProcedure.java
@@ -11,25 +11,19 @@ public class ROOBEEREffectEffectStartedappliedProcedure {
 			return;
 		if (Math.random() < 0.7) {
 			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-				_entity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 6000, 2));
-			if (Math.random() < 0.4) {
+				_entity.addEffect(new MobEffectInstance(MobEffects.HEAL, 1, 1));
+			if (Math.random() < 0.6) {
 				if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-					_entity.addEffect(new MobEffectInstance(MobEffects.SATURATION, 6000, 40));
+					_entity.addEffect(new MobEffectInstance(MobEffects.HEAL, 1, 1));
 				if (Math.random() < 0.5) {
 					if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-						_entity.addEffect(new MobEffectInstance(MobEffects.DIG_SPEED, 6000, 1));
-					if (Math.random() < 0.2) {
+						_entity.addEffect(new MobEffectInstance(MobEffects.HEAL, 1, 2));
+					if (Math.random() < 0.4) {
 						if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-							_entity.addEffect(new MobEffectInstance(MobEffects.NIGHT_VISION, 6000, 1));
+							_entity.addEffect(new MobEffectInstance(MobEffects.HEAL, 1, 2));
 					}
 				}
 			}
-		}
-		if (Math.random() < 0.2) {
-			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-				_entity.addEffect(new MobEffectInstance(MobEffects.CONFUSION, 6000, 1));
-			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-				_entity.addEffect(new MobEffectInstance(MobEffects.POISON, 500, 2));
 		}
 	}
 }
